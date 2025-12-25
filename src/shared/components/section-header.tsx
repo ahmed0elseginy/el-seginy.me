1f87b480d794c30e4cc112830242f7cc610e320d
Ahmed El-Seginy <141983816+ahmed0elseginy@users.noreply.github.com>
2025-12-26 00:44:04 +0200

    Update section-header.tsx

--- src/shared/components/section-header.tsx
+++ src/shared/components/section-header.tsx
@@ -42,7 +42,7 @@ export function SectionHeader({
         </motion.div>
       )}
 
-      <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-4 font-headline">
+      <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-4 font-headline text-white">
         {title}
         {subtitle && (
           <>
